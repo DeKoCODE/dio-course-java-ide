24507049b72b6865768024f9104fcb7de77ab372
Denison Kolling <denisonkolling@gmail.com>
2023-02-13 08:12:27 -0300

    first commit

--- src/br/com/dio/FirstJavaCode.java
+++ src/br/com/dio/FirstJavaCode.java
@@ -6,5 +6,41 @@ public class FirstJavaCode {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+        book = new Book(name:
+    }
+}
+
+class Book {
+    private String name;
+    private Integer numPages;
+
+    public Book(String name, Integer numPages) {
+        this.name = name;
+        this.numPages = numPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", numPages=" + numPages +
+                '}';
     }
 }
